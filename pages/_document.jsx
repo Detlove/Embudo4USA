f4af0ab9eb1337e2162a9cfbeabeb94d239b21c6
David Eusebio <davidetlove@gmail.com>
2022-03-21 11:58:38 -0500

    improve layout shift

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -4,12 +4,14 @@ export default function Document () {
   return (
     <Html>
       <Head>
+        <link rel='preconnect' href='https://fonts.gstatic.com' crossorigin />
         <link rel='preconnect' href='https://player.vimeo.com' />
         <link rel='preconnect' href='https://i.vimeocdn.com' />
         <link rel='preconnect' href='https://f.vimeocdn.com' />
         <link rel='preconnect' href='https://fresnel.vimeocdn.com' />
         <link rel='icon' type='image/x-icon' href='/favicon.ico' />
-        <link href='https://fonts.googleapis.com/css?family=Poppins:100,200,300,regular,500,600,700,800,900&display=swap' rel='stylesheet' />
+        <link rel='preload' as='style' href='https://fonts.googleapis.com/css?family=Poppins:200,300,regular,500,600,700,800&display=swap' />
+        <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Poppins:200,300,regular,500,600,700,800&display=swap' />
       </Head>
       <body>
         <Main />
