49d4989523380689f07768d9e4b69f16e5d8c5f0
David Eusebio <davidetlove@gmail.com>
2022-03-21 09:03:43 -0500

    typeofs

--- components/Landing/Landing.jsx
+++ components/Landing/Landing.jsx
@@ -14,7 +14,7 @@ export const Landing = () => {
       </Head>
       <Script src='/flipdown.js' strategy='afterInteractive' />
       <section className={styles.message}>
-        <p id='test'>Pero como lo bueno no dura para siempre y sabemos <strong>el valor que aporta este entrenamiento</strong>, necesitamos ponerle un tiempo limite</p>
+        <p id='test'>Pero como lo bueno no dura para siempre y sabemos <strong>el valor que aporta este entrenamiento</strong>, necesitamos ponerle un tiempo límite</p>
       </section>
       <section className={styles.sale} id='landing'>
         <div className={styles.sale_cont}>
@@ -26,13 +26,13 @@ export const Landing = () => {
           >
             <Image src='/assets/box.png' width={600} height={445} layout='responsive' />
           </picture>
-          <span className={styles.price}>UNICO PAGO DE <strong>$97 USD</strong></span>
+          <span className={styles.price}>ÚNICO PAGO DE <strong>$97 USD</strong></span>
           <span className={styles.price2}>o en 12 cuotas de <strong>$8 USD</strong></span>
           <button
             className={styles.button}
             onClick={() => goPurchase('btn2')}
           >
-            !ESTOY LISTO, PARA DUPLICAR MIS VENTAS!
+            !ESTOY LISTO, PARA IMPORTAR DESDE USA!
           </button>
           <picture className={styles.payments}>
             <Image
